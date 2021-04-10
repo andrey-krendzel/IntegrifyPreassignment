dd4b62364d885ca8cf229b96aaafbb7c160ca750
user <andreykrendzel@hotmail.com>
2021-04-10 12:50:07 +0300

    Final changes

--- src/Details.js
+++ src/Details.js
@@ -7,21 +7,24 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 
 function DetailsCard(props){
- console.log(props.employee.company)
+ 
   return(
 <div className="card card-size">
   
-  <div className="card-body">
+  <div className="card-body-left">
     <p className="card-text">- name: {props.employee.name}</p>
     <p className="card-text">- username: {props.employee.username}</p>
     <p className="card-text">- email: {props.employee.email}</p>
     <p className="card-text">- phone: {props.employee.phone}</p>
-    <p className="card-text">- company: {props.company.name} </p>
+    <p className="card-text">- company: {props.employee.company.name} </p>
     <p className="card-text">- website: {props.employee.website} </p>
-    <p className="card-text">- address: 
+    <div className="card-text">- address: 
     <ul>
-      <li>street: {props.employee.name}</li>
-    </ul> </p>
+      <li>street: {props.employee.address.street}</li>
+      <li>suite: {props.employee.address.suite}</li>
+      <li>city: {props.employee.address.city}</li>
+      <li>zipcode: {props.employee.address.zipcode}</li>
+    </ul> </div>
     <Button url="/" style="btn btn-primary" text="Go back" />
   </div>
 </div>
@@ -65,12 +68,16 @@ function Details() {
       .catch((error) => console.log(error));
   }, [update]);
 
+  if (employee.company == null || employee.address == null) {
+    return(<p>Loading...</p>)
+  } else {
+
   return (
 <div className="container">
 
 
 
-<DetailsCard employee={employee} company={employee.company} />
+<DetailsCard employee={employee} />
 
 
 
@@ -81,6 +88,7 @@ function Details() {
 
   
   );
+  }
 }
 
 export default Details;
